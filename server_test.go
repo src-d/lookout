936239de1a778f572b773aa7009f6b81d91e59a7
Maxim Sukharev <maxim@sourced.tech>
2018-07-20 18:31:16 +0200

    tests for server
    Signed-off-by: Maxim Sukharev <maxim@sourced.tech>

---
+++ server_test.go
@@ -0,0 +1,118 @@
+package lookout
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+	log "gopkg.in/src-d/go-log.v1"
+)
+
+func TestServerReview(t *testing.T) {
+	require := require.New(t)
+
+	log.DefaultLogger = log.New(log.Fields{"app": "lookout"})
+
+	watcher := &WatcherMock{}
+	poster := &PosterMock{}
+	fileGetter := &FileGetterMock{}
+	analyzers := map[string]AnalyzerClient{
+		"mock": &AnalyzerClientMock{},
+	}
+
+	srv := NewServer(watcher, poster, fileGetter, analyzers)
+	srv.Run(context.TODO())
+
+	reviewEvent := &ReviewEvent{
+		Provider:    "Mock",
+		InternalID:  "internal-id",
+		IsMergeable: true,
+		Source: ReferencePointer{
+			InternalRepositoryURL: "file:///test",
+			ReferenceName:         "feature",
+			Hash:                  "source-hash",
+		},
+		Merge: ReferencePointer{
+			InternalRepositoryURL: "file:///test",
+			ReferenceName:         "merge-branch",
+			Hash:                  "merge-hash",
+		},
+		CommitRevision: CommitRevision{
+			Base: ReferencePointer{
+				InternalRepositoryURL: "file:///test",
+				ReferenceName:         "master",
+				Hash:                  "base-hash",
+			},
+			Head: ReferencePointer{
+				InternalRepositoryURL: "file:///test",
+				ReferenceName:         "master",
+				Hash:                  "head-hash",
+			},
+		},
+	}
+
+	err := watcher.Send(reviewEvent)
+	require.Nil(err)
+
+	comments := poster.PopComments()
+	require.Len(comments, 1)
+	require.Equal(makeComment(reviewEvent.CommitRevision.Base, reviewEvent.CommitRevision.Head), comments[0])
+}
+
+type WatcherMock struct {
+	handler EventHandler
+}
+
+func (w *WatcherMock) Watch(ctx context.Context, e EventHandler) error {
+	w.handler = e
+	return nil
+}
+
+func (w *WatcherMock) Send(e Event) error {
+	return w.handler(e)
+}
+
+type PosterMock struct {
+	comments []*Comment
+}
+
+func (p *PosterMock) Post(_ context.Context, e Event, cs []*Comment) error {
+	p.comments = cs
+	return nil
+}
+
+func (p *PosterMock) PopComments() []*Comment {
+	cs := p.comments[:]
+	p.comments = []*Comment{}
+	return cs
+}
+
+type FileGetterMock struct {
+}
+
+func (g *FileGetterMock) GetFiles(_ context.Context, req *FilesRequest) (FileScanner, error) {
+	return nil, nil
+}
+
+type AnalyzerClientMock struct {
+}
+
+func (a *AnalyzerClientMock) NotifyReviewEvent(ctx context.Context, in *ReviewEvent, opts ...grpc.CallOption) (*EventResponse, error) {
+	return &EventResponse{
+		Comments: []*Comment{
+			makeComment(in.CommitRevision.Base, in.CommitRevision.Head),
+		},
+	}, nil
+}
+
+func (a *AnalyzerClientMock) NotifyPushEvent(ctx context.Context, in *PushEvent, opts ...grpc.CallOption) (*EventResponse, error) {
+	return nil, nil
+}
+
+func makeComment(from, to ReferencePointer) *Comment {
+	return &Comment{
+		Text: fmt.Sprintf("%s > %s", from.Hash, to.Hash),
+	}
+}
