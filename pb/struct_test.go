0283c98a87bcf31ea6f437474f595e4089bdbca2
Maxim Sukharev <maxim@sourced.tech>
2018-07-24 15:48:35 +0200

    add to pb.struct helper
    Signed-off-by: Maxim Sukharev <maxim@sourced.tech>

---
+++ pb/struct_test.go
@@ -0,0 +1,102 @@
+package pb_test
+
+import (
+	"testing"
+
+	types "github.com/gogo/protobuf/types"
+	"github.com/stretchr/testify/require"
+
+	"github.com/src-d/lookout/pb"
+)
+
+func TestToStruct(t *testing.T) {
+	require := require.New(t)
+
+	inputMap := map[string]interface{}{
+		"bool":   true,
+		"int":    1,
+		"string": "val",
+		"float":  0.5,
+		"nil":    nil,
+		"array":  []string{"val1", "val2"},
+		"map": map[string]int{
+			"field1": 1,
+		},
+		"struct": struct {
+			Val string
+		}{Val: "val"},
+	}
+
+	expectedSt := &types.Struct{
+		Fields: map[string]*types.Value{
+			"bool": &types.Value{
+				Kind: &types.Value_BoolValue{
+					BoolValue: true,
+				},
+			},
+			"int": &types.Value{
+				Kind: &types.Value_NumberValue{
+					NumberValue: 1,
+				},
+			},
+			"string": &types.Value{
+				Kind: &types.Value_StringValue{
+					StringValue: "val",
+				},
+			},
+			"float": &types.Value{
+				Kind: &types.Value_NumberValue{
+					NumberValue: 0.5,
+				},
+			},
+			"nil": nil,
+			"array": &types.Value{
+				Kind: &types.Value_ListValue{
+					ListValue: &types.ListValue{
+						Values: []*types.Value{
+							&types.Value{
+								Kind: &types.Value_StringValue{
+									StringValue: "val1",
+								},
+							},
+							&types.Value{
+								Kind: &types.Value_StringValue{
+									StringValue: "val2",
+								},
+							},
+						},
+					},
+				},
+			},
+			"map": &types.Value{
+				Kind: &types.Value_StructValue{
+					StructValue: &types.Struct{
+						Fields: map[string]*types.Value{
+							"field1": &types.Value{
+								Kind: &types.Value_NumberValue{
+									NumberValue: 1,
+								},
+							},
+						},
+					},
+				},
+			},
+			"struct": &types.Value{
+				Kind: &types.Value_StructValue{
+					StructValue: &types.Struct{
+						Fields: map[string]*types.Value{
+							"Val": &types.Value{
+								Kind: &types.Value_StringValue{
+									StringValue: "val",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	st := pb.ToStruct(inputMap)
+	require.Equal(expectedSt, st)
+}
