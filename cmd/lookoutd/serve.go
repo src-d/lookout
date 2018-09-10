b4921f2875b3f028916956e2c2c4d3dc5885ea23
Carlos Martín <carlos.martin.sanchez@gmail.com>
2018-09-10 18:26:53 +0200

    Make DB version log structured
    Signed-off-by: Carlos Martín <carlos.martin.sanchez@gmail.com>

--- cmd/lookoutd/serve.go
+++ cmd/lookoutd/serve.go
@@ -371,7 +371,7 @@ func (c *ServeCommand) initDB() (*sql.DB, error) {
 				"Use '%s migrate' to upgrade your database", dbVersion, version, build, maxVersion, name)
 	}
 
-	log.Debugf("the DB version is up to date, %v", dbVersion)
+	log.With(log.Fields{"db-version": dbVersion}).Debugf("the DB version is up to date")
 	log.Infof("connection with the DB established")
 	return db, nil
 }
