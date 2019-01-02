9192f3b2c1606837c0ab6afb5ad0e230dd617ebc
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2019-01-02 11:08:56 +0100

    Added a test for timeout error
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- cmd/server-test/error_analyzer_test.go
+++ cmd/server-test/error_analyzer_test.go
@@ -16,6 +16,8 @@ import (
 	"github.com/stretchr/testify/suite"
 )
 
+const dummyConfigFileWithTimeouts = "../../fixtures/dummy_config_with_timeouts.yml"
+
 type errAnalyzer struct{}
 
 func (a *errAnalyzer) NotifyReviewEvent(ctx context.Context, e *lookout.ReviewEvent) (*lookout.EventResponse, error) {
@@ -26,8 +28,23 @@ func (a *errAnalyzer) NotifyPushEvent(ctx context.Context, e *lookout.PushEvent)
 	return nil, errors.New("push error")
 }
 
+type sleepyErrAnalyzer struct{}
+
+func (a *sleepyErrAnalyzer) NotifyReviewEvent(ctx context.Context, e *lookout.ReviewEvent) (*lookout.EventResponse, error) {
+	time.Sleep(1 * time.Millisecond)
+	return nil, errors.New("review error")
+}
+
+func (a *sleepyErrAnalyzer) NotifyPushEvent(ctx context.Context, e *lookout.PushEvent) (*lookout.EventResponse, error) {
+	time.Sleep(1 * time.Millisecond)
+	return nil, errors.New("push error")
+}
+
 type ErrorAnalyzerIntegrationSuite struct {
 	IntegrationSuite
+	configFile string
+	analyzer   lookout.AnalyzerServer
+	errMessage string
 }
 
 func (suite *ErrorAnalyzerIntegrationSuite) startAnalyzer(ctx context.Context, a lookout.AnalyzerServer) error {
@@ -56,9 +73,9 @@ func (suite *ErrorAnalyzerIntegrationSuite) SetupTest() {
 	suite.ResetDB()
 
 	suite.StoppableCtx()
-	suite.r, suite.w = suite.StartLookoutd(dummyConfigFile)
+	suite.r, suite.w = suite.StartLookoutd(suite.configFile)
 
-	suite.startAnalyzer(suite.Ctx, &errAnalyzer{})
+	suite.startAnalyzer(suite.Ctx, suite.analyzer)
 	suite.GrepTrue(suite.r, `msg="connection state changed to 'READY'" addr="ipv4://localhost:9930" analyzer=Dummy`)
 }
 
@@ -72,9 +89,18 @@ func (suite *ErrorAnalyzerIntegrationSuite) TearDownTest() {
 func (suite *ErrorAnalyzerIntegrationSuite) TestAnalyzerErr() {
 	suite.sendEvent(successJSON)
 
-	suite.GrepTrue(suite.r, `msg="analysis failed" analyzer=Dummy app=lookoutd error="rpc error: code = Unknown desc = review error"`)
+	suite.GrepTrue(suite.r, suite.errMessage)
 }
 
 func TestErrorAnalyzerIntegrationSuite(t *testing.T) {
-	suite.Run(t, new(ErrorAnalyzerIntegrationSuite))
+	suite.Run(t, &ErrorAnalyzerIntegrationSuite{
+		configFile: dummyConfigFile,
+		analyzer:   &errAnalyzer{},
+		errMessage: `msg="analysis failed: code: Unknown - message: review error" analyzer=Dummy app=lookoutd error="rpc error: code = Unknown desc = review error"`,
+	})
+	suite.Run(t, &ErrorAnalyzerIntegrationSuite{
+		configFile: dummyConfigFileWithTimeouts,
+		analyzer:   &sleepyErrAnalyzer{},
+		errMessage: `msg="analysis failed: timeout exceeded, try increasing analyzerReviewTimeout" analyzer=Dummy app=lookoutd error="rpc error: code = DeadlineExceeded desc = context deadline exceeded"`,
+	})
 }
